8090d12c8ffc11fdd06bf40e1dc2a4c5ddfe7902
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-11 15:14:29 +0000

    Added a css, fixed style and corrected brackets and ;

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.getElementById('click-me') .addEventListener('click',secondButton)
+document.getElementById('click-me').addEventListener('click', secondButton)
 
-document.getElementById('button-two') .addEventListener('click',buttonOn)
+document.getElementById('button-two').addEventListener('click', buttonOn)
 
 function secondButton () {
   document.getElementById('paragraph').innerHTML = 'Done!'
